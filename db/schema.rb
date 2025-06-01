034550b87f8f0865bb8c8a956bac527d4227ce59
skgc32 <191213129+skgc32@users.noreply.github.com>
2025-06-01 20:45:30 +0900

    チャットルーム見出し

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_23_142803) do
     t.datetime "updated_at", null: false
     t.string "profile"
     t.string "profile_image_id"
-    t.datetime "last_sign_in_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
